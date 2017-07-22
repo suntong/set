a7faec18765fcb5ffd056ef553aa70c4312f8ade
Tong Sun <suntong@cpan.org>
2017-07-21 23:47:24 -0400

    - [+] add comments and case-insensitive SET

--- example_test.go
+++ example_test.go
@@ -9,27 +9,56 @@ import (
 // for standalone test, change package to `main` and the next func def to,
 // func main() {
 func ExampleSet() {
-	s1 := set.NewSet()
-	s1.Add("Apple")
-	s1.Add("Orange")
-	fmt.Println(s1.Has("Apple"))
-	fmt.Println(s1.Has("Grape"))
+	{
+		// == case-sensitive SET
+		s1 := set.NewSet()
+		s1.Add("Apple")
+		s1.Add("Orange")
+		fmt.Println(s1.Has("Apple"))
+		fmt.Println(s1.Has("Grape"))
 
-	s2 := set.NewSetFrom([]string{"Apple", "Orange"})
-	fmt.Println(s2.Has("Apple"))
-	s2.Add("Grape")
-	fmt.Println(s2.Has("Grape"))
-	s2.Del("Grape")
-	fmt.Println(s2.Has("Grape"))
+		s2 := set.NewSetFrom([]string{"Apple", "Orange"})
+		fmt.Println(s2.Has("Apple"))
+		s2.Add("Grape")
+		fmt.Println(s2.Len(), s2.Has("Grape"), s2.Has("grape"))
+		s2.Del("Grape")
+		fmt.Println(s2.Len(), s2.Has("Grape"))
+		fmt.Println(s2.Get())
+	}
+	fmt.Println()
+
+	{
+		// == case-insensitive SET
+		s1 := set.NewSet0()
+		s1.Add("Apple")
+		s1.Add("Orange")
+		fmt.Println(s1.Has("Apple"))
+		fmt.Println(s1.Has("Grape"))
+
+		s2 := set.NewSet0From([]string{"Apple", "Orange"})
+		fmt.Println(s2.Has("Apple"))
+		s2.Add("Grape")
+		fmt.Println(s2.Len(), s2.Has("Grape"), s2.Has("grape"))
+		s2.Del("Grape")
+		fmt.Println(s2.Len(), s2.Has("Grape"))
+		fmt.Println(s2.Get())
+	}
 
 	// Output:
 	// true
 	// false
 	// true
+	// 3 true false
+	// 2 false
+	// [Apple Orange]
+	//
 	// true
 	// false
+	// true
+	// 3 true true
+	// 2 false
+	// [apple orange]
 }
 
 // To show the full code in GoDoc
-type dummy struct {
-}
+var s set.Set = set.NewSet()
--- set.go
+++ set.go
@@ -8,6 +8,7 @@ package set
 
 import (
 	"sort"
+	"strings"
 )
 
 type Set interface {
@@ -18,13 +19,22 @@ type Set interface {
 	Get() []string
 }
 
+// set is case-sensitive SET
 type set map[string]struct{}
 
-func (s set) Add(a string)      { s[a] = struct{}{} }
-func (s set) Del(a string)      { delete(s, a) }
-func (s set) Has(a string) bool { _, ok := s[a]; return ok }
-func (s set) Len() int          { return len(s) }
+// Add adds the given string to the set.
+func (s set) Add(a string) { s[a] = struct{}{} }
 
+// Del deletes the given string to the set. CAUTION! No key validation!
+func (s set) Del(a string) { delete(s, a) }
+
+// Has tells if the given string is in the set.
+func (s set) Has(a string) bool { _, ok := s[a]; return ok }
+
+// Len returns the number of elements in the set.
+func (s set) Len() int { return len(s) }
+
+// Get returns the keys of the set
 func (s set) Get() []string {
 	var keys []string
 	for k := range s {
@@ -35,13 +45,56 @@ func (s set) Get() []string {
 	return keys
 }
 
+// NewSet returns a case-sensitive SET
 func NewSet() set {
 	s := make(set)
 	return s
 }
 
+// NewSetFrom returns a case-sensitive SET from the given string slice
 func NewSetFrom(i []string) set {
 	s := NewSet()
+	for _, v := range i {
+		s.Add(v)
+	}
+	return s
+}
+
+// set0 is case-insensitive SET
+type set0 map[string]struct{}
+
+// Add adds the given string to the set.
+func (s set0) Add(a string) { a = strings.ToLower(a); s[a] = struct{}{} }
+
+// Del deletes the given string to the set. CAUTION! No key validation!
+func (s set0) Del(a string) { a = strings.ToLower(a); delete(s, a) }
+
+// Has tells if the given string is in the set.
+func (s set0) Has(a string) bool { a = strings.ToLower(a); _, ok := s[a]; return ok }
+
+// Len returns the number of elements in the set.
+func (s set0) Len() int { return len(s) }
+
+// Get returns the keys of the set
+func (s set0) Get() []string {
+	var keys []string
+	for k := range s {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	return keys
+}
+
+// NewSet0 returns a case-sensitive SET
+func NewSet0() set0 {
+	s := make(set0)
+	return s
+}
+
+// NewSet0From returns a case-sensitive SET from the given string slice
+func NewSet0From(i []string) set0 {
+	s := NewSet0()
 	for _, v := range i {
 		s.Add(v)
 	}
